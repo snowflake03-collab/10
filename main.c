cebf499e942aeb5327c2e08566b0a184c361b482
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-11-09 09:11:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,9 @@
 
 int main(void)
 {
-    char src[] = "The worst things to eat before you sleep";
-    char dst[100];
+    char str[30] = "happy C programming";
     
-    strcpy(dst, src);
-    
-    printf("copied string: %s\n", dst);
+    printf("문자열 \"%s\"의 길이: %i\n", str, strlen(str)); 
     
   system("PAUSE");	
   return 0;
