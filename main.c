4c3a1e0eacc8b28d996fc89541f2ffdca4c51fca
snowflake03-collab <snowflake03@sookmyung.ac.kr>
2025-11-09 09:33:00 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,24 @@
 
 int main(void)
 {
-    char str[30] = "happy C programming";
     
-    printf("문자열 \"%s\"의 길이: %i\n", str, strlen(str)); 
+    char input[100];
+    FILE * fp;
+    int i;
     
+    //fopen
+    fp = fopen("sample.txt", "w");
+    
+    for(i = 0; i < 3; i++){
+    //fprintf
+      printf("input a word: ");
+      scanf("%s", input);
+      fprintf(fp, "%s\n", input); 
+}
+    //fclose
+    fclose(fp);
+    
+
   system("PAUSE");	
   return 0;
 }
